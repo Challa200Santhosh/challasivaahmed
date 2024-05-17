aeb23460656bfa6516f1dcf648e5ee95f21dd80b
mohammedashraf11 <135623997+mohammedashraf11@users.noreply.github.com>
2024-05-17 09:13:25 +0000

    change print statement challa #2

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main()
 {
-   int num1 ,num2,result;
+   int num1 ,num2,result,result1,result2;
    printf("Enter the numbers:  " " ");
    scanf("%d %d",&num1,&num2);
    result = num1 - num2;
    result1 = num1 * num2;
    result2 = num1 / num2;
 
-   printf("Sum of %d and %d is %d\n",num1 ,num2,result);
-   printf("Sum of %d and %d is %d\n",num1 ,num2,result1);
-   printf("Sum of %d and %d is %d\n",num1 ,num2,result2);
+   printf("Sub of %d and %d is %d\n",num1 ,num2,result);
+   printf("product of %d and %d is %d\n",num1 ,num2,result1);
+   printf("ratio of %d and %d is %d\n",num1 ,num2,result2);
    
    
     return 0;
