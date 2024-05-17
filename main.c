3eb03916659bb819828603206c0ac725b919912e
mohammedashraf11 <135623997+mohammedashraf11@users.noreply.github.com>
2024-05-17 09:41:01 +0000

    removed excess code

--- main.c
+++ main.c
@@ -11,12 +11,6 @@ int main()
    printf("Sub of %d and %d is %d\n",num1 ,num2,result);
    printf("product of %d and %d is %d\n",num1 ,num2,result1);
    printf("ratio of %d and %d is %d\n",num1 ,num2,result2);
-   if(num1%2==0){
-    print("even");
-   }
-   else{
-    print("odd");
-   }
    
     return 0;
 }
